ef2465aaddeff8c6428d8dfa8e37ad840237caed
polidog <polidogs@gmail.com>
2014-01-21 02:21:00 +0900

    composer update

--- DependencyInjection/ChatworkExtension.php
+++ DependencyInjection/ChatworkExtension.php
@@ -25,7 +25,7 @@ class ChatworkExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('service.yml');
+        $loader->load('services.yml');
         if (!empty($config['chatwork']['apikey'])) {
             $container->get('chatwork_client')->authenticate($config['chatwork']['apikey']);
         }
